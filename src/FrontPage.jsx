43fb498748e31a516b66958ca77c9b1656d0208a
Katlin Anderson and kerriann walker <katlin+kerriann@>
2019-07-03 10:44:19 -0700

    Create WorksForYou Component

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignUp from './SignUp';
+import WorksForYou from './WorksForYou'
 
 function FrontPage() {
     return (
         <div>
             <SignUp />
+            <WorksForYou/>
         </div>
     );
 }
---
+++ src/WorksForYou.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Find from './assets/magnifying-glass.png';
+import Research from './assets/speech-bubble.png';
+import Compare from './assets/invoice.png';
+import Apply from './assets/briefcase.png';
+
+const worksStyles = {
+    backgroundColor: '#f5f6f7',
+    margin: '0'
+};
+
+function WorksForYou() {
+    return (
+        <div id='work-panel' style = {worksStyles}>
+            <h1>How Glassdoor Works For You</h1>
+                <div id='works'>
+                <div id ='find'>
+                    <img className = 'smallIcon' src={Find}></img>
+                    <h3>Find the Right Jobs</h3>
+                </div>
+                <div id ='research'>
+                    <img className= 'smallIcon' src={Research}></img>
+                    <h3>Research Companies</h3>
+                </div>
+                <div id='compare'>
+                    <img className= 'smallIcon' src={Compare}></img>
+                    <h3>Compare Salaries</h3>
+                </div>
+                <div id='apply'>
+                    <img className= 'smallIcon' src= {Apply}></img>
+                    <h3>Apply to Jobs</h3>
+                </div>
+            <style jsx>{`
+            .smallIcon {
+                width: 20%;
+            }
+            #works{
+                display: flex;
+            }
+            h1{
+                margin: 0px;
+                padding: 21px;
+            }
+            `}</style>
+            </div>
+        </div>
+    );
+}
+
+export default WorksForYou;
