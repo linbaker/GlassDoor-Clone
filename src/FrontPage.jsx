7cc21aad76e2211dc05d38ea68ae308d3927f00d
Katlin Anderson and kerriann walker <katlin+kerriann@>
2019-07-03 11:43:12 -0700

    add Header Component to Front Page

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import SignUp from './SignUp';
 import WorksForYou from './WorksForYou'
 import Explore from './Explore';
+import Header from './Header';
 
 function FrontPage() {
     return (
         <div>
+             <Header/>
             <SignUp />
             <WorksForYou/>
             <Explore/>
---
+++ src/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const headStyles = {
+
+}
+
+function Header() {
+    return (
+        <div id='head-panel' style = {headStyles}>
+            <h1>The Real GrassDoor</h1>
+            <h5>Find a Job</h5>
+            <h5>For Employers</h5>
+            <button class ='buttons'>Sign In</button>
+            <button class='buttons'> Post Jobs Free</button>
+            <style jsx>{`
+             #head-panel{
+                display: flex;
+                justify-content: space-evenly;
+            }
+            h1{
+                margin: 0px;
+                padding: 21px;
+            }
+            .buttons{
+                background-color: #00000080;
+                border: none;
+                color: white;
+                text-align: center;
+                font-size: 15px;
+                border-radius: 10px;
+            }
+            `}</style>
+        
+        </div>
+    );
+}
+
+export default Header;
